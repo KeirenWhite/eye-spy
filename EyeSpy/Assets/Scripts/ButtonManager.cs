a363f7e402d3874bf2eb3a73f25ef6d8dff1dc0c
KeirenWhite <143643209+KeirenWhite@users.noreply.github.com>
2025-07-25 15:21:35 -0400

    figuring out how to lay ground work

---
+++ EyeSpy/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+/*[System.Serializable]
+
+public class ButtonDisplayPair
+{
+    public Sprite itemIcon;
+    public Sprite display;
+    public string displayText;
+}*/
+public class ButtonManager : MonoBehaviour
+{
+    /*public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;*/
+
+    public GameObject button1;
+
+    public GameObject display1;
+    public GameObject display2;
+    public GameObject display3;
+    public GameObject display4;
+    public GameObject display5;
+    public GameObject display6;
+
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+    public GameObject text5;
+    public GameObject text6;
+
+
+    public void OnClick()
+    {
+        /*button1.onClick.AddListener(() =>
+        {
+            display1.SetActive(true);
+            text1.SetActive(true);
+        });*/
+    }
+
+
+
+
+
+
+
+
+}
+
